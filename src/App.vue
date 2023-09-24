cae8ae6e87a09ad86b36a43045e354064bfd5486
“yesenia <aryesigon@gmail.com>
2023-09-24 05:38:34 -0300

    Formulario de contacto con iconos de adornos dentro de vue.js

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div id="app">
+<Formulariodecontacto/>
+</div>
+</template>
+<script>
+import Formulariodecontacto from './components/Formulariodecontacto.vue'
+export default { 
+name: 'app',
+components: {
+     // eslint-disable-next-line
+Formulariodecontacto
+}
+}
+</script>
+  
+
+
+
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+createApp (App).mount ('#app')
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.css'
+
